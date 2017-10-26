1d16ca25cf55a9cfabd653070086bcb59f8bd0a6
Tehuel <torresbaldi.tehuel@gmail.com>
2017-10-25 23:12:54 -0300

    modify build script
    add bennugd path as parameter
    add prg compilation at the end

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
+cd "${0%/*}"
+
+# get bennu binaries directory parameter, or set default
+BGD_RUNTIME=${1:-"bennugd-binaries/linux"}
+
+# add bennugd to system path
+export PATH=$BGD_RUNTIME/bin:$PATH
+export LD_LIBRARY_PATH=$BGD_RUNTIME/lib:$LD_LIBRARY_PATH
 
 # concatenate every prg file into one new prg
 cat prg/globals.prg prg/functions.prg prg/main.prg > fpgtools.prg
-
+bgdc fpgtools.prg
